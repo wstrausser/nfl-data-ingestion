b2192b8e616ff6171640d11bb45e7a920cece997
William Strausser <william.e.strausser@gmail.com>
2023-10-29 17:36:49 +0000

    Initial commit

---
+++ .devcontainer/post_start_command.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+pip install --upgrade pip
+git config --global --add safe.directory /workspaces/math-100-code
+git config --global init.defaultBranch main
+git config --global core.autocrlf false
+
+sudo apt install libsnappy-dev
+pip install nfl_data_py
