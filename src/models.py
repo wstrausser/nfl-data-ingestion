8c13f91a98e6724a80baf12dad7f9b156fb9c767
William E. Strausser <william.e.strausser@gmail.com>
2023-11-03 05:09:37 +0000

    Altered schema to enable relational ingestion of teams separate from games

--- src/models.py
+++ src/models.py
@@ -1,12 +1,25 @@
-from datetime import datetime
-from sqlalchemy import String
+from datetime import (
+    date,
+    datetime,
+    time,
+)
+from sqlalchemy import (
+    ForeignKey,
+    String,
+    Text,
+    select,
+)
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     mapped_column,
+    relationship,
 )
-from typing import Optional
+from typing import (
+    List,
+    Optional,
+)
 
 from src.global_variables import METADATA_OBJ
 
@@ -17,28 +30,54 @@ class Base(DeclarativeBase):
 
 
 # Tables
+class Team(Base):
+    __tablename__ = "teams"
+
+    team_id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(50))
+    name: Mapped[str] = mapped_column(String(50))
+    abbreviation: Mapped[str] = mapped_column(String(3))
+    conference: Mapped[str] = mapped_column(String(3))
+    division: Mapped[str] = mapped_column(String(10))
+    color_1: Mapped[str] = mapped_column(String(7))
+    color_2: Mapped[str] = mapped_column(String(7))
+    color_3: Mapped[str] = mapped_column(String(7))
+    color_4: Mapped[str] = mapped_column(String(7))
+    logo: Mapped[str] = mapped_column(Text)
+    wordmark: Mapped[str] = mapped_column(Text)
+
+    team_home_games: Mapped[List["Game"]] = relationship("Game", back_populates="game_home_team", foreign_keys="Game.home_team_id")
+    team_away_games: Mapped[List["Game"]] = relationship("Game", back_populates="game_away_team", foreign_keys="Game.away_team_id")
+
+
 class Game(Base):
     __tablename__ = "games"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    game_id: Mapped[int] = mapped_column(primary_key=True)
     result_updated: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True))
-    game_id: Mapped[str] = mapped_column(String(20))
+    api_game_id: Mapped[str] = mapped_column(String(20))
     season: Mapped[int]
     week: Mapped[int]
-    home_team: Mapped[str] = mapped_column(String(3))
-    away_team: Mapped[str] = mapped_column(String(3))
-    result: Mapped[Optional[str]] = mapped_column(String(3))
+    game_date: Mapped[date]
+    game_start_time: Mapped[time]
+    home_team_id: Mapped[int] = mapped_column(ForeignKey("teams.team_id"))
+    away_team_id: Mapped[int] = mapped_column(ForeignKey("teams.team_id"))
+    home_score: Mapped[Optional[int]]
+    away_score: Mapped[Optional[int]]
+
+    game_home_team: Mapped[Team] = relationship("Team", back_populates="team_home_games", foreign_keys="Game.home_team_id")
+    game_away_team: Mapped[Team] = relationship("Team", back_populates="team_away_games", foreign_keys="Game.away_team_id")
 
     def exists(self, session):
         q = session.query(Game).filter(
-            Game.game_id == self.game_id,
+            Game.api_game_id == self.api_game_id,
         )
         exists = session.query(q.exists()).first()[0]
         return exists
 
     def existing_record(self, session):
         q = session.query(Game).filter(
-            Game.game_id == self.game_id,
+            Game.api_game_id == self.api_game_id,
         )
         existing_record = q.first()
         return existing_record
